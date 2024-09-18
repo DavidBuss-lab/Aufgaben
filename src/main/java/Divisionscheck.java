c740b913f27d87207e16a0618bf2a89df002d6e0
DABUSS <david.buss@btc-it-services.com>
2024-09-18 11:27:41 +0200

    Divisionscheck

---
+++ src/main/java/Divisionscheck.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Divisionscheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Welcher Divisor soll geprüft werden?");
+        int divisor = sc.nextInt();
+        System.out.println("Bis zu welcher Zahl?");
+        int max = sc1.nextInt();
+        int Zahl = 0;
+        int Ergebnis = Zahl / divisor;
+        while (Ergebnis <= max)
+        {
+            if(Ergebnis % divisor == 0){
+            System.out.println(Ergebnis);
+            Ergebnis = Ergebnis + 1;}
+                else{
+                    Ergebnis = Ergebnis + 1;
+        }
+    }
+}}
