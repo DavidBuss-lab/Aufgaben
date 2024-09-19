2b5bb0c0f098b13a9e8d6b72e551755a4ef7248b
DABUSS <david.buss@btc-it-services.com>
2024-09-19 09:44:15 +0200

    Divisionscheck 1.2

--- src/main/java/Divisionscheck.java
+++ src/main/java/Divisionscheck.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Divisionscheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Scanner sc1 = new Scanner(System.in);
+
         System.out.println("Welcher Divisor soll geprüft werden?");
         int divisor = sc.nextInt();
+
         System.out.println("Bis zu welcher Zahl?");
-        int max = sc1.nextInt();
-        int Ergebnis = 0;
-        while (Ergebnis <= max)
-        {
-            if(Ergebnis % divisor == 0){
-                System.out.println(Ergebnis);
-                Ergebnis = Ergebnis + 1;}
-            else{
-                Ergebnis = Ergebnis + 1;
+        int max = sc.nextInt();
+
+        int ergebnis = 0;
+        while (ergebnis <= max) {
+            if (ergebnis % divisor == 0) {
+                System.out.println(ergebnis);
             }
+                ergebnis = ergebnis + 1;
         }
-    }}
+    }
+}
