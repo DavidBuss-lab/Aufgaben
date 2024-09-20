c26f1b13f7f77f64346555a13bd38598dbb43e8d
DABUSS <david.buss@btc-it-services.com>
2024-09-20 11:44:04 +0200

    while update 1.0

--- src/main/java/ArithmetischeOperationen.java
+++ src/main/java/ArithmetischeOperationen.java
@@ -2,11 +2,13 @@ public class ArithmetischeOperationen {
     public static void main(String[] args) {
         int Var1 = 10;
         int Var2 = 10;
-        if(++Var1 == Var2++) {
+        while (++Var1 == Var2++) {
             System.out.println("Die Zahlen sind gleich");
+            break;
         }
-        else{
+        while (++Var1 != Var2++) {
             System.out.println("Es ist nicht dasselbe");
+            break;
         }
         System.out.println("Variable 1 (++Var) ist: " + Var1);
         System.out.println("Variable 2 (Var++) ist: " + Var2);
