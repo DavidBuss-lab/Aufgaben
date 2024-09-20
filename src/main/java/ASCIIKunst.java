63c80b576e93d53cbc14a3d647172299a53408d5
DABUSS <david.buss@btc-it-services.com>
2024-09-20 07:42:30 +0200

    rückwärtsAusgeben 1.2 und Updates

--- src/main/java/ASCIIKunst.java
+++ src/main/java/ASCIIKunst.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ASCIIKunst {
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Wähle zwichen Viereck (1) und Dreieck (2) aus:");
+        int Auswahl = sc1.nextInt();
+
+    }
 }
