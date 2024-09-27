7e81e77d86a4337b24539dca22d08794ec60df77
DABUSS <david.buss@btc-it-services.com>
2024-09-27 10:38:30 +0200

    Türmestapeln 1.0

--- src/main/java/w02/FibonacciRekursiv.java
+++ src/main/java/w02/FibonacciRekursiv.java
@@ -4,17 +4,16 @@ import java.util.Scanner;
 
 public class FibonacciRekursiv {
 
-    public static int fibonacci(int [] reihe, int n) {
-        if (n < reihe.length - 2){
+    public static int fibonacci(int[] reihe, int n) {
+        if (n < reihe.length - 2) {
             reihe[n + 2] = reihe[n] + reihe[n + 1];
             n++;
             return fibonacci(reihe, n);
-            }
-
-        else {
+        } else {
             return n;
         }
     }
+
     public static void main(String[] args) {
         int zahl1 = 0;
         int zahl2 = 1;
@@ -22,19 +21,15 @@ public class FibonacciRekursiv {
         Scanner scan = new Scanner(System.in);
         System.out.println("Gebe die Menge der Fibonaccizahlen ein, die berechnet werden sollen");
         int max = scan.nextInt();
-        int [] ergebnis = new int[max];
+        int[] ergebnis = new int[max];
         ergebnis[0] = 0;
         ergebnis[1] = 1;
         int n = 0;
 
-        fibonacci(ergebnis,n);
+        fibonacci(ergebnis, n);
 
-        for (int i = 0; i < max; i++){
+        for (int i = 0; i < max; i++) {
             System.out.print(ergebnis[i] + " ");
         }
-
-
-
-
     }
 }
